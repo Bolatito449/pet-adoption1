79f9d63a7906e6638c7ed7041ed86d1cab3f881f
Bolatito449 <bolatitoadegoroye00@gmail.com>
2025-06-24 11:56:26 +0100

    infra pipeline correction

--- output.tf
+++ output.tf
@@ -1,9 +1,7 @@
-output "sonarqube_public_ip" {
-  value = module.sonarqube.sonarqube_public_ip
-}
 output "bastion-public-ip" {
   value = module.bastion.bastion_public_ip
 }
+
 output "nexus_ip" {
   value = module.nexus.nexus_ip
 }
@@ -11,3 +9,14 @@ output "nexus_private_ip" {
   value = module.nexus.nexus_private_ip
 }
 
+output "ansible-server-ip" {
+  value = module.ansible.ansible_ip
+}
+
+output "sonarqube_public_ip" {
+  value = module.sonarqube.sonarqube_public_ip
+}
+
+output "DB-endpoint" {
+  value = module.database.db_endpoint
+}
