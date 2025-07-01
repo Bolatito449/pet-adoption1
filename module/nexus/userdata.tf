61e8adbd3a5958ad94f85c221834d869ef969147
Bolatito449 <bolatitoadegoroye00@gmail.com>
2025-07-01 14:15:49 +0100

    nexus userdata update

--- module/nexus/userdata.tf
+++ module/nexus/userdata.tf
@@ -1,49 +1,4 @@
 
-# locals {
-# userdata = <<EOF
-# #!/bin/bash
-# sudo yum update -y
-# sudo yum install wget -y
-# sudo yum install java-1.8.0-openjdk.x86_64 -y
-# sudo mkdir /app && cd /app
-# sudo wget https://download.sonatype.com/nexus/3/nexus-3.66.0-02-unix.tar.gz
-# sudo tar -xvf nexus-3.66.0-02-unix.tar.gz
-# sudo mv nexus-3.66.0-02 nexus
-# sudo adduser nexus
-# sudo chown -R nexus:nexus /app/nexus
-# sudo chown -R nexus:nexus /app/sonatype-work
-# sudo cat <<EOT> /app/nexus/bin/nexus.rc
-# run_as_user="nexus"
-# EOT
-# sed -i '2s/-Xms2703m/-Xms512m/' /app/nexus/bin/nexus.vmoptions
-# sed -i '3s/-Xmx2703m/-Xmx512m/' /app/nexus/bin/nexus.vmoptions
-# sed -i '4s/-XX:MaxDirectMemorySize=2703m/-XX:MaxDirectMemorySize=512m/' /app/nexus/bin/nexus.vmoptions
-# sudo touch /etc/systemd/system/nexus.service
-# sudo cat <<EOT> /etc/systemd/system/nexus.service
-# [Unit]
-# Description=nexus service
-# After=network.target
-# [Service]
-# Type=forking
-# LimitNOFILE=65536
-# User=nexus
-# Group=nexus
-# ExecStart=/app/nexus/bin/nexus start
-# ExecStop=/app/nexus/bin/nexus stop
-# User=nexus
-# Restart=on-abort
-# [Install]
-# WantedBy=multi-user.target
-# EOT
-# sudo ln -s /app/nexus/bin/nexus /etc/init.d/nexus
-# sudo chkconfig --add nexus
-# sudo chkconfig --levels 345 nexus on
-# sudo service nexus start
-# #curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh | bash && sudo NEW_RELIC_API_KEY=${var.nr-key} NEW_RELIC_ACCOUNT_ID=${var.nr-id} /usr/local/bin/newrelic install -y
-# sudo hostnamectl set-hostname Nexus
-# EOF
-# }
-
 locals {
   userdata = <<EOF
 #!/bin/bash
@@ -67,12 +22,17 @@ After=network.target
 
 [Service]
 Type=forking
-LimitNOFILE=65536
 User=nexus
 Group=nexus
-ExecStart=/app/nexus/bin/nexus start
-ExecStop=/app/nexus/bin/nexus stop
-Restart=on-abort
+WorkingDirectory=/app/nexus
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+Environment="JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64"
+Environment="INSTALL4J_ADD_VM_PARAMS=-Xms1024M -Xmx2048M -XX:MaxDirectMemorySize=1024M"
+ExecStart=/bin/bash /app/nexus/bin/nexus start
+ExecStop=/bin/bash /app/nexus/bin/nexus stop
+TimeoutStartSec=300
+LimitNOFILE=65536
+Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
@@ -86,3 +46,5 @@ curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh |
 sudo hostnamectl set-hostname Nexus
 EOF
 }
+
+
