77d70ac552b3dd7c9d7e4687a83573af74f901cf
Bolatito449 <bolatitoadegoroye00@gmail.com>
2025-07-02 12:21:37 +0100

    token update

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 provider "vault" {
   address = "https://vault.bolatitoadegoroye.top"
-  token   = "s.x3Az0Wudwv4O4u6yIXyu81Of"
+  token   = "s.amm7lmQakqbkD7pySH07igDd"
 }
 
 terraform {
@@ -13,5 +13,14 @@ terraform {
     key          = "infrastructure/terraform.tfstate"
     region       = "eu-west-2"
     use_lockfile = true
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.45.0"
+    }
   }
 }
